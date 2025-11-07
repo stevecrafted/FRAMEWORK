567f6453d074385f498021df1534d4c85967c393
steve-daniel0 <stevens.rakotomalala@gmail.com>
2025-11-07 16:07:29 +0300

    sprint4

--- src/main/java/org/Entity/ClassMethodUrl.java
+++ src/main/java/org/Entity/ClassMethodUrl.java
@@ -1,6 +1,10 @@
 package org.Entity;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletResponse;
 
 public class ClassMethodUrl {
     Class<?> Class;
@@ -25,5 +29,23 @@ public class ClassMethodUrl {
 
     public void SetMethod(Method m) {
         this.Method = m;
+    }
+
+    public void ExecuteMethode(HttpServletResponse resp) throws IOException {
+        try {
+            Object controller = this.Class.getDeclaredConstructor().newInstance();
+            Object result = this.Method.invoke(controller);
+
+            if (result instanceof String) {
+                String viewName = (String) result;
+
+                resp.getWriter().write("Resutltat du fonction \n");
+                resp.getWriter().write(viewName);
+            }
+
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/FrontServlet.java
+++ src/main/java/org/FrontServlet.java
@@ -6,7 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
@@ -34,6 +37,8 @@ public class FrontServlet extends HttpServlet {
         System.out.println("---------- Sauvegarde des url ----------");
         CmuUtils.saveCmuList(reflections, urlMappings);
 
+        ServletContext context = getServletContext();
+        context.setAttribute("urlMappings", urlMappings);
     }
 
     @Override
@@ -45,11 +50,30 @@ public class FrontServlet extends HttpServlet {
         resp.setContentType("text/plain; charset=UTF-8");
         resp.getWriter().write("FrontServlet a reçu : " + req.getRequestURL() + "\n");
         if (cmu != null) {
-            resp.getWriter().write(
-                    "Cette url existe dans la classe " + cmu.getMyClass() + " dans la methode " + cmu.getMyMethod());
+
+            printToClient(resp, "Cette url existe dans la classe " + cmu.getMyClass() + " dans la methode "
+                    + cmu.getMyMethod());
+
+            // Jerena ny type de retour any
+            // Raha String dia Executena Tenenina ho type string io
+            if (cmu.getMyMethod().getReturnType() == String.class) {
+                printToClient(resp, "Cette methode renvoie un String\n");
+                // Execution anle methode
+                cmu.ExecuteMethode(resp);
+            }
+            // Raha != String sy ModelView
+            else {
+                printToClient(resp, "Sady tsy String no tsy Model View");
+            }
+
+            // Raha Model view
+            // Averina ilay test.jsp
         } else {
             resp.getWriter().write("Error 404");
         }
     }
 
+    public void printToClient(HttpServletResponse resp, String message) throws IOException {
+        resp.getWriter().write(message);
+    }
 }
